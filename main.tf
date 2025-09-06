da9005a6ea062ca4ff57c9d20f18a8a254f33477
Apps402 <36360376+Apps402@users.noreply.github.com>
2025-09-06 15:39:06 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,28 +1,28 @@
 terraform {
-    required_providers{
-        azurerm = {
-                source = "hashicorp/azurerm"
-                version = "4.42"
-                }
-    backend "azurerm"{
-        resource_group_name = "backendrgap"
-        storage_account_name = "backendstgap19"
-        container_name = "apstate"
-        key = "state.tf"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.42.0"
     }
+  }
+
+  backend "azurerm" {
+    resource_group_name   = "backendrgap"
+    storage_account_name  = "backendstgap19"
+    container_name        = "apstate"
+    key                   = "state.tf"
+  }
 }
 
-provider "azurerm"{
-    features{}
-    subscription_id = "9f199aee-1ad7-464f-bd74-4b88ef990086"
+provider "azurerm" {
+  features {}
+  subscription_id = "9f199aee-1ad7-464f-bd74-4b88ef990086"
 }
-
-
 
 resource "azurerm_resource_group" "rg01" {
-    name = "Appsrg"
-    location = "West Europe"
-  
+  name     = "Appsrg"
+  location = "West Europe"
 }
+
 
 
