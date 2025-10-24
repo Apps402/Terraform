23685b2dbd79e8ef1d154f757a68614d6a8d401b
Apps402 <36360376+Apps402@users.noreply.github.com>
2025-10-24 23:53:35 +0530

    Update main.tf

--- modules/sql_db/main.tf
+++ modules/sql_db/main.tf
@@ -2,6 +2,10 @@ resource "azurerm_mssql_database" "example" {
   name                = var.sql_db
   resource_group_name = var.rg_name
   location            = var.sql_dbloc
-  server_name         = var.sql_db_id
-
+  server_id         = var.sql_db_id
+  collation    = "SQL_Latin1_General_CP1_CI_AS"
+  license_type = "LicenseIncluded"
+  max_size_gb  = "2"
+  sku_name     = "S0"
+  enclave_type = "VBS"
 }
