96039395f865d9d9027bc22230a354c3822ab7bf
Apps402 <36360376+Apps402@users.noreply.github.com>
2025-12-04 19:49:42 +0530

    Update main.tf

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -10,11 +10,11 @@ module "rg" {
     rgs = var.rgs   
 }
 
-module "acr" {
-    depends_on = [ module.rg ]
-    source = "../../modules/container_registry"
-    acr = var.acr
-}
+#module "acr" {
+ #   depends_on = [ module.rg ]
+  #  source = "../../modules/container_registry"
+   # acr = var.acr
+#}
 
 module "sql" {
    depends_on = [ module.rg ]
@@ -24,12 +24,12 @@ module "sql" {
 }
 
 
-module "aks_cluster" {
-    depends_on = [ module.rg ]
-    source = "../../modules/kubernetes cluster"
-    aks = var.aks
+#module "aks_cluster" {
+ #   depends_on = [ module.rg ]
+  #  source = "../../modules/kubernetes cluster"
+   # aks = var.aks
   
-}
+#}
 
 #module "vnet" {
  #   depends_on = [ module.rg ]
